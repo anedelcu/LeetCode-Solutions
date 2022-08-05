5a512e80736688f653d7574ca6f053fd66775198
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-08-05 09:45:28 -0500

    Time: 6 ms (42.25%), Space: 56.5 MB (30.34%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,20 +1,13 @@
 class Solution {
     public static int majorityElement(int[] nums) {
-        Map<Integer, Integer> myMap = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            if(myMap.containsKey(nums[i])) {
-                myMap.put(nums[i],myMap.get(nums[i]) + 1 );
-            }
-            else {
-                myMap.put(nums[i], 1);
-            }
+        // Base case...
+        if (nums.length == 1) {
+            return nums[0];
         }
-        int majElem = 0;
-        for(int key : myMap.keySet()){
-            if(myMap.get(key) > nums.length / 2) {
-                majElem = key;
-            }
-        }
-        return majElem;
+        // Sort nums array...
+        Arrays.sort(nums);
+        // Since the majority element appears more than n / 2 times...
+        // The n/2 -th element in the sorted nums must be the majority element...
+        return nums[nums.length / 2];
     }
 }
