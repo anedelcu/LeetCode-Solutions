0d12b524868e53c623861f8ce1278629265e5c0a
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-08-11 19:28:46 -0500

    Time: 11 ms (44.29%), Space: 103.9 MB (28.88%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -8,6 +8,43 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+     
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        slow = reverse(slow);
+        fast = head;
+        while(slow != null ) {
+            if(slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode reverse = null;
+        while(head != null) {
+            reverse = new ListNode(head.val, reverse);
+            head = head.next;
+        }
+        return reverse;
+    }
+}
+
+
+
+
+
+/*
 class Solution {
     public boolean isPalindrome(ListNode head) {
      
@@ -33,3 +70,4 @@ class Solution {
         return reverse;
     }
 }
+*/
