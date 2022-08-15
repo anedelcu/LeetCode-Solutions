971553334074638fd285776bd72a168b86f65024
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-08-15 13:09:19 -0500

    Time: 37 ms (49.96%), Space: 57.9 MB (83.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,31 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> resList = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res =new LinkedList<>();
-        for(int i=0;i<nums.length - 2;i++){
-		//when numbers are same
-            if(i>0&& nums[i]==nums[i-1])
-                continue;
-            
-            int l=i+1,r =nums.length-1;
-            while(r>l){
-                int threeSum= nums[i]+nums[l]+nums[r];
-				//reduce right pointer
-                if(threeSum>0)
-                    r--;
-				//reduce left pointer
-                else if(threeSum <0)
-                    l++;
-                else{
-                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    l++;
-					//Find the next non repeating element
-                    while (nums[l]==nums[l-1] && l<r)
-                        l++;
+        
+        
+        for(int i = 0; i < n - 2; i ++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i -1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = 0 - nums[i];
+                
+                while(low < high) {
+                    if(nums[low] + nums[high] == sum) {
+                        resList.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        low++;
+                        /*
+                        List<Integer> res = new ArrayList<>();
+                        res.add(nums[i]);
+                        res.add(nums[low]);
+                        res.add(nums[high]);
+                        resList.add(res);
+                        */
+                        while(low < high && nums[low] == nums[low - 1]) {
+                            low++;
+                        }
+                        
+                    }
+                    else if(nums[low] + nums[high] > sum) {
+                        high--;
+                    }
+                    else {
+                        low++;
+                    }       
                 }
             }
-            
         }
-        return res;
+        
+        return resList;
     }
 }
