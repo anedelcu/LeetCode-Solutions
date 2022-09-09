aa69a572263f1c3c6401f1754b19dbc62b481faf
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-09-09 09:23:59 -0500

    Time: 2 ms (76.86%), Space: 44.9 MB (40.14%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,25 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        Queue<List<Integer>> permutations = new LinkedList<>();
-        permutations.add(new ArrayList<>());
-        
-        for(int num : nums) {
-            int n = permutations.size();
-            for(int i = 0; i < n; i++) {
-                List<Integer> oldPermutation = permutations.poll();
-                for(int j = 0; j <= oldPermutation.size(); j++) {
-                    List<Integer> newPermutation = new ArrayList<Integer>(oldPermutation);
-                    newPermutation.add(j, num);
-                    if(newPermutation.size() == nums.length) {
-                        result.add(newPermutation);
-                    }
-                    else {
-                        permutations.add(newPermutation);
-                    }
-                }
-            }
-        }
+        permuteRec(nums, 0, new ArrayList<Integer>(), result);
         return result;
+    }
+    
+    private void permuteRec(int[] nums, int index, List<Integer> currPermut, List<List<Integer>> result) {
+        if(currPermut.size() == nums.length) {
+            result.add(currPermut);
+        }
+        else {
+            for(int i = 0; i <= currPermut.size(); i++ ) {
+                List<Integer> newPermut = new ArrayList<>(currPermut);
+                newPermut.add(i, nums[index]);
+                permuteRec(nums, index + 1, newPermut, result);
+            }            
+        }
     }
 }
