4f65c72625bac59732e2c36b2488d49382509e62
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-09-26 14:42:45 -0500

    Time: 61 ms (73.22%), Space: 64.8 MB (64.62%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -2,12 +2,20 @@ class Solution {
     public String kthLargestNumber(String[] nums, int k) {
         PriorityQueue<String> minHeap = new PriorityQueue<String>((s1, s2) -> s1.length() != s2.length() ? s1.length() - s2.length() : s1.compareTo(s2));
         
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < k; i++) {
             minHeap.add(nums[i]);
-            if(minHeap.size() > k) {
+        }
+        for(int i = k; i < nums.length; i++) {
+            if(nums[i].length() == minHeap.peek().length() && nums[i].compareTo(minHeap.peek()) >= 0) {
                 minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+            else if(nums[i].length() > minHeap.peek().length()) {
+                minHeap.poll();
+                minHeap.add(nums[i]);
             }
         }
+        
         return minHeap.peek();
         
     }
