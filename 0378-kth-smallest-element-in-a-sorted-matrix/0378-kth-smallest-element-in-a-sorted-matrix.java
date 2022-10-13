279319b7c67c468573d4cc999f6260ebf042af08
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-10-12 19:55:43 -0500

    Time: 53 ms (13.06%), Space: 58.7 MB (5.72%) - LeetHub

---
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Node> minHeap = new PriorityQueue<Node>(
+        (n1, n2) -> matrix[n1.rowIndex][n1.colIndex] - matrix[n2.rowIndex][n2.colIndex]);
+        
+        for(int i = 0; i < matrix.length; i++) {
+            minHeap.add(new Node(i, 0));
+        }
+        
+        int numberCount = 0;
+        int result = 0;
+        System.out.println(minHeap.size());
+        System.out.println(matrix[minHeap.peek().rowIndex][minHeap.peek().colIndex]);
+        System.out.println(minHeap.peek().rowIndex);
+        System.out.println(minHeap.peek().colIndex);
+        while(!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
+            result = matrix[node.rowIndex][node.colIndex];
+            numberCount++;
+            if(numberCount == k) {
+                break;
+            }
+            node.colIndex++;
+            if(node.colIndex < matrix[node.rowIndex].length) {
+                minHeap.add(node);
+            }
+        }
+        return result;
+    }
+}
+
+
+class Node {
+  int rowIndex;
+  int colIndex;
+
+  Node(int rowIndex, int colIndex) {
+    this.rowIndex = rowIndex;
+    this.colIndex = colIndex;
+  }
+}
