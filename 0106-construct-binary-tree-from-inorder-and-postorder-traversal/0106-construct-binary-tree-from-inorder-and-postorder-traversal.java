b5e7e55f86d82568a9f5be51dcc24b3dcd11218f
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2022-10-17 16:52:00 -0500

    Time: 4 ms (69.54%), Space: 44.6 MB (31.42%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    int [] inOrder;
+    int [] postOrder;
+    Map<Integer, Integer> map = new HashMap<>();
+    int index;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        inOrder = inorder;
+        postOrder = postorder;
+        int n = inorder.length;
+        index = n - 1;
+        
+        for(int  i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+        
+        TreeNode root =  dfs(0, n -1);
+        
+        return root;
+        
+    }
+    
+    private TreeNode dfs(int start, int end) {
+        // base case
+        if(start > end) {
+            return null;
+        }
+        //build the current node
+        int currVal = postOrder[index--];
+        TreeNode curr = new TreeNode(currVal);
+        
+        // build right subtree
+        curr.right = dfs(map.get(currVal) + 1, end);
+        
+        // build left subtree
+        curr.left = dfs(start, map.get(currVal) - 1);
+        
+        // return the current node
+        return curr;
+    }
+}
